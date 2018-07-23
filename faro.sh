3aa04780d09dfc35f6cbf53249c951cc95302588
NoobTersakiti <41572821+NoobTersakiti@users.noreply.github.com>
2018-07-23 10:33:52 +0000

    Add files via upload

---
+++ faro.sh
@@ -0,0 +1,76 @@
+#!/system/xbin/bash
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f standard -F gay "Faro Tools"
+sleep 2
+echo " $blue [+] ============================================ [+]
+	Name tools: Faro
+
+	Author : Nealfaro
+
+	Code's : bash
+
+	Team : DemonCyberArmy
+
+	Kontak Me :  0895635105583
+
+[+] ============================================ [+]"
+echo
+echo "\033[32;1mSilahkan Pilih Tools nya :)"
+echo "\033[36;1m"
+echo $green "1=> Spam Sms Tokopedia"
+echo
+echo $purple "2=> Spam Sms Telkomnyet"
+echo
+echo $red "3=> Nuyul Bitcoin"
+echo
+echo $blue "0=> exit"
+echo
+echo "\033[32;1m"
+read -p "root#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "faro tools" | lolcat
+pkg install php
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "faro tools"
+pkg install php
+php t.php
+fi
+
+if
+[ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "faro tools" | lolcat
+sleep 1
+pkg install php
+pkg install wget
+wget -O tuyul.php https://pastebin.com/raw/3ERZV3nL
+mv tuyul.php $HOME
+php $HOME/tuyul.php
+fi
+
+
+if
+[ $bro = 0 ] || [ $bro = 0 ]
+then
+echo "Kembali lagi yah :v"
+echo "klo gk kembali w eue :v"
+sleep 1
+exit
+fi
